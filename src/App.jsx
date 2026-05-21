99e3a0672ddbd7f01549d38ad4424718139e644d
RanjithKumarKarnan <ranjithkumar31072002@gmail.com>
2026-05-21 12:35:22 +0530

    Feat : updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-center">Hello World</h1>
-      <p className="text-center">CI/CD Working Successfully</p>
+     <div className="flex flex-col items-center justify-center min-h-screen">
+       <h1 className="text-center text-2xl font-bold">Developing By <a href="https://rancotechnology.com" className='text-blue-800' target="_blank" rel="noopener noreferrer">Ranco Tech</a></h1>
+       <p className="text-center text-lg">CI/CD Working Successfully</p>
+     </div>
     </>
   )
 }
